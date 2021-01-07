ee281a7f730103390a82927d5ff91f7b34ea61ef
Fabian Lee <flee999@hotmail.com>
2021-01-07 14:38:13 +0000

    renamed repo

--- tests/roles/pull_roles.sh
+++ tests/roles/pull_roles.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/fabianlee/ansible-role-chrony.git
 git clone https://github.com/fabianlee/ansible-role-domain-join.git
 git clone https://github.com/fabianlee/ansible-sssd.git
-git clone https://github.com/fabianlee/ansible-role-system-dns.git
+git clone https://github.com/fabianlee/ansible-role-systemd-dns.git
