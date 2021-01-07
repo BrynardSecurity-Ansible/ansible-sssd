27732e13c858a39b51cc820382cbcb9a4c930139
Fabian Lee <flee999@hotmail.com>
2021-01-07 14:23:14 +0000

    pull roles required for playbook

---
+++ tests/roles/pull_roles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/fabianlee/ansible-role-chrony.git
+git clone https://github.com/fabianlee/ansible-role-domain-join.git
+git clone https://github.com/fabianlee/ansible-sssd.git
+git clone https://github.com/fabianlee/ansible-role-system-dns.git
