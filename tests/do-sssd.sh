492278115926c549b0df01c1a3d09f47b2d6f4ce
Fabian Lee <flee999@hotmail.com>
2021-02-05 18:18:34 +0000

    removed 'extravar_' prefix from default valued used for token

--- tests/do-sssd.sh
+++ tests/do-sssd.sh
@@ -7,8 +7,8 @@ set -ex
 ansible all -m ping
 
 echo "uncomment next line for secure LDAP SSSD"
-#ansible-playbook ansible-sssd-playbook-ad-ldap-only.yml -l ldap_managed --extra-vars "extravar_ldap_default_authtok=MyP4ss!"
+#ansible-playbook ansible-sssd-playbook-ad-ldap-only.yml -l ldap_managed --extra-vars "ldap_default_authtok=MyP4ss!"
 
 echo "uncomment next line for secure LDAP with Kerberos SSSD"
-#ansible-playbook ansible-sssd-playbook-ad-ldap-plus-krb5.yml -l ldap_krb5_managed --extra-vars "extravar_ldap_default_authtok=MyP4ss! kerberos_user_password=MyP4ss!"
+#ansible-playbook ansible-sssd-playbook-ad-ldap-plus-krb5.yml -l ldap_krb5_managed --extra-vars "ldap_default_authtok=MyP4ss! kerberos_user_password=MyP4ss!"
 
